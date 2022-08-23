12d30d116570984692d1fb389c7510bba4fbefb0
DeclanODonovan <102086761+DeclanODonovan@users.noreply.github.com>
2022-08-23 12:39:14 -0500

    Started BlueJ Chptr 1 notes

---
+++ My First BlueJ Project/chapter1Notes.java
@@ -0,0 +1,42 @@
+
+/**
+ * Chapter 1 notes for APCS
+ * where we are going to take some code notes
+ * Helped by: John "Jack" Charles Campbell II
+ * 
+ * @author (Declan O'Donovan)
+ * @version (8/23/22)
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class chapter1Notes
+{
+ 
+
+    /**
+     * main method
+     * where we are going to code
+     */
+    public static void main(String[] args)
+    {
+       Scanner input = new Scanner(System.in);
+       
+       //these are the basic data types
+       int x = 9,y,z;
+       double num = 5, num1 = 23.4567, num2;
+       char letter = 'd';
+       boolean choice = false;
+       float anum = 7;
+       long a = 9;
+       short b = 5;
+       
+       //These are objects!
+       String word = new String("Declan O'Donovan");
+       Integer number = new Integer(47);
+       Double number2 = new Double(34.5);
+       
+       
+    }
+}
